504da52676185228470d71a8c4fd41b50b1391a7
hansololiviakim <dev.hansolkim@gmail.com>
2023-03-25 03:57:04 +0900

    Refactor: 함수 세분화하여 분리

--- hansol/color-flipper.js
+++ hansol/color-flipper.js
@@ -3,12 +3,17 @@ const hexElements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 function btnClickHandler() {
   let hexcode = '#';
   for(let i = 1; i <= 6; i++) {
-    hexcode += hexElements[Math.floor(Math.random() * hexElements.length)];
+    hexcode += hexElements[getRandomNumber()];
   }
+  
   document.querySelector('.content h1 span:first-child').innerText = `Background Color : `;
   document.querySelector('.content h1 span:last-child').innerText = `${hexcode}`;
   document.body.style.backgroundColor = hexcode;
   document.querySelector('.content h1 span:last-child').style.color = hexcode;
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hexElements.length)
 }
 
 btnClickHandler();
